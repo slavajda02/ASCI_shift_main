77a20eb6cf2b8635c703db59013f43bf05ac3504
slavajda02 <slavajda0@gmail.com>
2022-12-04 19:48:14 +0100

    Adde GUI and dynamic logic

--- app.py
+++ app.py
@@ -2,5 +2,15 @@ import customtkinter
 
 class app(customtkinter.CTk):
     def __init__(self):
-        super.__init__()
+        super().__init__()
+        self.title("Encryptor")
+        
+        tabview = customtkinter.CTkTabview(self)
+        self.tabview.add("Encryption")  # add tab at the end
+        self.tabview.add("Decryption")  # add tab at the end
+        self.tabview.add("Keygen")  # add tab at the end
+        self.tabview.set("Encryption")  # set currently visible tab
+        
+        self.button_1 = customtkinter.Button(tabview.tab("tab 1"))
+        
         

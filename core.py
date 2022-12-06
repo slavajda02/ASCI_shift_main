f2df8e0b7b2c2b333b0dff26ba4f7b9c6ed708f7
slavajda02 <slavajda0@gmail.com>
2022-12-06 18:39:47 +0100

    coreDef

--- core.py
+++ core.py
@@ -1,2 +1,18 @@
-def create():
-    print("Hello world")
+def Encrypt(string, key):
+    asciiL = list(string.encode('ascii'))
+    for x in asciiL:
+        
+    
+    
+def Decrypt(string, key):
+    
+    
+    
+def KeyGenerator():
+    
+    
+    
+    
+    
+def KeyConvert(key):
+    
